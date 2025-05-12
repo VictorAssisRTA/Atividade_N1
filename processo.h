7cf7c0bc2c0d1d8ba211acb38386d8d748b75e96
Victor_RTA <117090218+VictorAssisRTA@users.noreply.github.com>
2025-05-12 15:48:01 -0300

    Update processo.h

--- processo.h
+++ processo.h
@@ -1,27 +1,23 @@
 #ifndef PROCESSO_H
 #define PROCESSO_H
 
-#define MAX_PROCESSOS 1000
-#define MAX_ASSUNTOS 10
-#define MAX_DATA 11
-
 typedef struct {
     int id;
     char numero[30];
-    char data_ajuizamento[MAX_DATA];
+    char data_ajuizamento[20];
     int id_classe;
-    int id_assunto[MAX_ASSUNTOS];
+    int id_assuntos[10]; 
     int num_assuntos;
     int ano_eleicao;
 } Processo;
 
-int ler_csv(const char *nome_arquivo, Processo processos[]);
-void escrever_csv(const char *nome_arquivo, Processo processos[], int total);
-void ordenar_por_id(Processo processos[], int total);
-void ordenar_por_data_desc(Processo processos[], int total);
-int contar_por_classe(Processo processos[], int total, int id_classe);
-int contar_assuntos_unicos(Processo processos[], int total);
-void listar_multiplos_assuntos(Processo processos[], int total);
-void calcular_dias_em_tramitacao(Processo processos[], int total);
+
+int ler_processos(const char *nome_arquivo, Processo processos[], int max);
+void ordenar_por_id(Processo processos[], int n);
+void ordenar_por_data(Processo processos[], int n);
+int contar_por_classe(Processo processos[], int n, int classe);
+int contar_assuntos_distintos(Processo processos[], int n);
+void listar_multiplos_assuntos(Processo processos[], int n);
+int calcular_dias_tramitando(const char *data);
 
 #endif
