d695e3ee31b6ebf57cc2a41d09af00e25c06238f
Victor_RTA <117090218+VictorAssisRTA@users.noreply.github.com>
2025-05-12 15:47:34 -0300

    Update processo.c

--- processo.c
+++ processo.c
@@ -4,70 +4,48 @@
 #include <time.h>
 #include "processo.h"
 
-int ler_csv(const char *nome_arquivo, Processo processos[]) { 
+int ler_processos(const char *nome_arquivo, Processo processos[], int max) {
     FILE *fp = fopen(nome_arquivo, "r");
-    if (!fp) { 
-        perror("Erro ao abrir arquivo");
-        exit(1);
-    }
-
+    if (!fp) return 0;
+    
     char linha[256];
-    fgets(linha, sizeof(linha), fp); 
+    int count = 0;
+    fgets(linha, sizeof(linha), fp); // Pular cabeçalho
 
-    int i = 0;
-    while (fgets(linha, sizeof(linha), fp) && i < MAX_PROCESSOS)
-    {
+    while (fgets(linha, sizeof(linha), fp) && count < max) {
+        Processo p;
         char *token = strtok(linha, ",");
-        processos[i].id = atoi(token);
+        p.id = atoi(token);
 
         token = strtok(NULL, ",");
-        strcpy(processos[i].data_ajuizamento, token);
+        strcpy(p.numero, token);
 
         token = strtok(NULL, ",");
-        processos[i].id_classe = atoi(token);
+        strcpy(p.data_ajuizamento, token);
 
-        token = strtok(NULL, ",");
-        processos[i].num_assuntos = 0;
+        token = strtok(NULL, ",{}\\n");
+        p.id_classe = atoi(token);
 
-        if (token[0] == '{') { 
-            token++;
-            while (*token && *token != "}") { 
-                processos[i].id_assunto[processos[i].num_assuntos++] = atoi(token);
-                while (*token && *token != ',' && *token != '}') token++;
-                if (*token == ',') token++;
-                
-            }
-        } else {
-            processos[i].id_assunto[0] = atoi(token);
-            processos[i].num_assuntos = 1;
+        token = strtok(NULL, "{}\\n");
+        p.num_assuntos = 0;
+        char *subtok = strtok(token, ",");
+        while (subtok) {
+            p.id_assuntos[p.num_assuntos++] = atoi(subtok);
+            subtok = strtok(NULL, ",");
         }
 
-        token = strtok(NULL, ",\n");
-        processos[i].ano_eleicao = atoi(token);
-        i++;
-    }
+        token = strtok(NULL, ",\\n");
+        p.ano_eleicao = atoi(token);
 
-    fclose(fp);
-    return i;
-}
-
-void escrever_csv(const char *arquivo_n, Processo processos[], int total) { 
-    FILE *fp = fopen(arquivo_n, "w");
-    fprintf(fp, "id,numero,data_ajuizamento,id_classe,id_assunto,ano_eleicao\n");
-    for (int i = 0; i < total; i++) { 
-        fprintf(fp, "%d,%s,%s,%d,{", processos[i].id, processos[i].numero, processos[i].data_ajuizamento, processos[i].id_classe);
-        for (int j = 0; j < processos[i].num_assuntos; j++) { 
-            fprintf(fp, "%d", processos[i].id_assunto[j]);
-            if (j < processos[i].num_assuntos - 1) fprintf(fp, ",");
-        }
-        fprintf(fp, "},%d\n", processos[i].ano_eleicao);
+        processos[count++] = p;
     }
     fclose(fp);
+    return count;
 }
 
-void ordenar_por_id(Processo processos[], int total) { 
-    for (int i = 0; i < total - 1; i++) { 
-        for (int j = i + 1; j < total; j++) { 
+void ordenar_por_id(Processo processos[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
             if (processos[i].id > processos[j].id) {
                 Processo tmp = processos[i];
                 processos[i] = processos[j];
@@ -77,14 +55,14 @@ void ordenar_por_id(Processo processos[], int total) {
     }
 }
 
-int comparar_data(const char *data1, const char *data2) { 
-    return strcmp(data2, data1);
+int comparar_data(const char *d1, const char *d2) {
+    return strcmp(d1, d2);
 }
 
-void ordenar_por_data_desc(Processo processos[], int total) { 
-    for (int i = 0; i < total - 1; i++) { 
-        for (int j = i + 1; j < total; j++) { 
-            if (comparar_data(processos[i].data_ajuizamento, processos[j].data_ajuizamento) > 0) { 
+void ordenar_por_data(Processo processos[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (comparar_data(processos[i].data_ajuizamento, processos[j].data_ajuizamento) < 0) {
                 Processo tmp = processos[i];
                 processos[i] = processos[j];
                 processos[j] = tmp;
@@ -93,50 +71,59 @@ void ordenar_por_data_desc(Processo processos[], int total) {
     }
 }
 
-int contar_por_classe(Processo processos[], int total, int id_classe) { 
+int contar_por_classe(Processo processos[], int n, int classe) {
     int count = 0;
-    for (int i = 0; i < total; i++) { 
-        if (processos[i].id_classe == id_classe) count++;
+    for (int i = 0; i < n; i++) {
+        if (processos[i].id_classe == classe)
+            count++;
     }
     return count;
 }
 
-int contar_assuntos_unicos(Processo processos[], int total) { 
-    int assuntos[10000] = {0}, count = 0;
-    for (int i = 0; i < total; i++) { 
+int contar_assuntos_distintos(Processo processos[], int n) {
+    int assuntos[10000] = {0};
+    int total = 0;
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < processos[i].num_assuntos; j++) {
-            int id = processos[i].id_assunto[j];
-            if (!assuntos[id]) { 
+            int id = processos[i].id_assuntos[j];
+            if (!assuntos[id]) {
                 assuntos[id] = 1;
-                count++;
+                total++;
             }
         }
     }
-    return count;
+    return total;
 }
 
-void listar_multiplos_assuntos(Processo processos[], int total) { 
-    printf("\nProcessos com múltiplos assuntos:\n");
-    for (int i = 0; i < total; i++) { 
+void listar_multiplos_assuntos(Processo processos[], int n) {
+    for (int i = 0; i < n; i++) {
         if (processos[i].num_assuntos > 1) {
-            printf("ID: %d, Número: %s\n", processos[i].id, processos[i].numero);
+            printf("Processo ID %d tem %d assuntos.\n", processos[i].id, processos[i].num_assuntos);
         }
-    } 
+    }
 }
 
-void calcular_dias_em_tramitacao(Processo processos[], int total) { 
-    time_t t = time(NULL);
-    struct tm *hoje = localtime(&t);
-    char buf[11];
-    strftime(buf, sizeof(buf), "%Y-%m-%d", hoje);
+int calcular_dias_tramitando(const char *data) {
+    struct tm tm = {0};
+    strptime(data, "%Y-%m-%d %H:%M:%S", &tm);
+    time_t data_ajuiz = mktime(&tm);
+    time_t hoje = time(NULL);
+    double diff = difftime(hoje, data_ajuiz);
+    return diff / (60 * 60 * 24);
+}
 
-    for (int i = 0; i < total; i++) { 
-        struct tm dt = {0};
-        sscanf(processos[i].data_ajuizamento, "%4d-%2d-%2d", &dt.tm_year, &dt.tm_mon, &dt.tm_mday);
-        dt.tm_year -= 1900;
-        dt.tm_mon -= 1;
-        time_t data_ini = mktime(&dt);
-        double dias = difftime(t, data_ini) / (60 * 60 * 24);
-        printf("Processo %s está em tramitação há %.0f dias\n", processos[i].numero, dias);
+void salvar_csv(const char *nome_arquivo, Processo processos[], int n) {
+    FILE *fp = fopen(nome_arquivo, "w");
+    if (!fp) return;
+    fprintf(fp, "id,numero,data_ajuizamento,id_classe,id_assunto,ano_eleicao\n");
+    for (int i = 0; i < n; i++) {
+        fprintf(fp, "%d,%s,%s,%d,", processos[i].id, processos[i].numero, processos[i].data_ajuizamento, processos[i].id_classe);
+        fprintf(fp, "{");
+        for (int j = 0; j < processos[i].num_assuntos; j++) {
+            fprintf(fp, "%d", processos[i].id_assuntos[j]);
+            if (j < processos[i].num_assuntos - 1) fprintf(fp, ",");
+        }
+        fprintf(fp, "},%d\n", processos[i].ano_eleicao);
     }
+    fclose(fp);
 }
